59091ebd3725542add270f5ff27526e2c5366018
Right Air Black <55693652+RightAirBlack@users.noreply.github.com>
2020-12-19 21:04:58 +0800

    Add files via upload

---
+++ src/org/right/ntlang/NTInstruction.java
@@ -0,0 +1,21 @@
+package org.right.ntlang.runtime;
+
+//一条指令
+public class NTInstruction {
+    public NTInstruction(NTOpcode opcode, int operand0) {
+        this(opcode, operand0, 0);
+    }
+    public NTInstruction(NTOpcode opcode, int operand0, int operand1) {
+        this.opcode = opcode;
+        this.operand0 = operand0;
+        this.operand1 = operand1;
+    }
+    public NTInstruction(NTOpcode opcode, String opvalue) {
+        this.opcode = opcode;
+        this.opvalue = opvalue;
+    }
+    public NTOpcode opcode = NTOpcode.forValue(0); //指令类型
+    public int operand0; //指令值1
+    public int operand1; //指令值2
+    public String opvalue; //指令值
+}
